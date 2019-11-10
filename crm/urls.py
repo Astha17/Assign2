f840bcce17c715cc9e812fe9d84404574ef5a409
Astha <adhakal@unomaha.edu>
2019-11-10 11:26:07 -0600

    New Customer

--- crm/urls.py
+++ crm/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     re_path(r'^home/$', views.home, name='home'),
     path('customer_list', views.customer_list, name='customer_list'),
     path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/create/', views.customer_new, name='customer_new'),
     path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
     path('service_list', views.service_list, name='service_list'),
     path('service/create/', views.service_new, name='service_new'),
